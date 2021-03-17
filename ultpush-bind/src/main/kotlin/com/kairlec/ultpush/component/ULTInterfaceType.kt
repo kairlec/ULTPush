37a72aba9f56d89f299f71ade7cfcff33789f042
Kairlec <sunfokairlec@gmail.com>
2021-03-17 15:39:20 +0800

    Remove unnecessary file

--- ultpush-bind/src/main/kotlin/com/kairlec/ultpush/component/ULTInterfaceType.kt
+++
@@ -1,5 +0,0 @@
-package com.kairlec.ultpush.component
-
-@Target(AnnotationTarget.FIELD, AnnotationTarget.PROPERTY_GETTER, AnnotationTarget.PROPERTY)
-@Retention(AnnotationRetention.RUNTIME)
-annotation class ULTInterfaceType()
